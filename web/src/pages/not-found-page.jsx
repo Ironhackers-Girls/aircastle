747cf9c3126c34e56c5f5b97d8e318a47b39f07e
Sara Garcia <saragpwork95@gmail.com>
2025-03-12 20:37:20 +0000

    feat: ui not found done

--- web/src/pages/not-found-page.jsx
+++ web/src/pages/not-found-page.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 import Lottie from "react-lottie";
-import animationData from "../assets/not-found-animation.json"; 
+import animationData from "../assets/not-found-animation.json";
 
 const NotFoundPage = () => {
   const lottieOptions = {
@@ -13,22 +13,24 @@ const NotFoundPage = () => {
   };
 
   return (
-    <div className="font-montserrat flex flex-col items-center justify-center p-10 sm:p-20 md:p-32 lg:p-40 xl:p-48 text-black text-center">
+    <div className="flex flex-col items-center justify-center p-10 sm:p-20 md:p-32 lg:p-40 xl:p-48 text-[var(--black)] text-center">
       <div className="flex justify-center items-center mb-5 w-full max-w-[500px]">
         <Lottie
           options={lottieOptions}
           height={250}
-          width="300px"  
+          width="300px"
           style={{ cursor: "default" }}
         />
       </div>
-      <h1 className="text-3xl sm:text-4xl font-bold mb-5 text-black">404 - Not Found</h1>
-      <p className="text-lg font-semibold mb-5 text-dark-gray">
+      <h1 className="text-3xl sm:text-4xl font-bold mb-5 text-[var(--black)]">
+        404 - Not Found
+      </h1>
+      <p className="text-lg font-semibold mb-5 text-[var(--dark-gray)]">
         Oops! The page you're looking for doesn't exist.
       </p>
       <Link
         to="/"
-        className="mt-2 px-6 py-3 bg-purple text-white text-xs sm:text-sm font-semibold rounded-full transform hover:scale-105 hover:shadow-xl"
+        className="mt-2 px-6 py-3 bg-[var(--purple)] text-[var(--white)] text-xs sm:text-sm font-semibold rounded-full transform hover:scale-105 hover:shadow-xl transition-all duration-300"
       >
         Go Back Home
       </Link>
